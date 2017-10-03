22b5b582f7a3c1a7f1c29f73bd431362fa0601b4
Nancy Olson <nancyolson531@gmail.com>
2017-10-03 11:29:53 -0500

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -26,20 +26,25 @@
 
   <?php include dirname(__FILE__)."/header.php"; ?>
 
-  <div class="container-fluid export">
+  <div class="container-fluid dashboard">
     <div class="row">
-      <div class="col-md-1">
-
+      <div class="col-md-2">
+        <h2>DASHBOARD</h2>
       </div>
 
-      <div class="col-md-9">
+      <div class="col-md-8">
+        <div class="sub-heading">Here is what <span class="highlight">YOU</span> have going on</div>
          
       </div>
 
       <div class="col-md-2">
-        <div class="sidebar">
-          <a class="btn btn-primary btn-ask btn-lg" href="#footer" role="button">ASK A QUESTION</a>
-          <a class="btn btn-primary btn-dashbrd btn-lg" href="/step-1.php" role="button">EXPORT A VEHICLE</a>
+        <a class="btn btn-primary btn-ask btn-lg" href="#footer" role="button">ASK A QUESTION</a>
+        <a class="btn btn-primary btn-lg" href="step-1.php" role="button">EXPORT A VEHICLE</a>
+        <div class="input-group">
+          <input type="text" class="form-control search" placeholder="Type what you are looking for here...">
+          <span class="input-group-btn">
+            <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+          </span>
         </div>
       </div>
     </div>
