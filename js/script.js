8dcd70bc9f3f2eb8e3776de334cb915a5193ce7a
Nancy Olson <nancyolson531@gmail.com>
2017-10-03 22:36:12 -0500

    border colors and phone/fax format

--- js/script.js
+++ js/script.js
@@ -86,4 +86,9 @@ $(document).ready(function() {
       }, 500);
   });
 
+  $('input[name*=phone], input[name*=fax]').on('input', function(e) {
+    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+  });
+
 });
