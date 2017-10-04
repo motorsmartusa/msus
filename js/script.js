de5fafa7895908221e92e6cbe8294227ea4fc4ef
Nancy Olson <nancyolson531@gmail.com>
2017-10-03 22:43:38 -0500

    disable submit on enter key

--- js/script.js
+++ js/script.js
@@ -57,6 +57,18 @@ $(document).ready(function() {
     }
   });
 
+  $('input[name*=phone], input[name*=fax]').on('input', function(e) {
+    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+  });
+
+  $(this).keypress(function(e) {
+    if(e.which == 13) {
+        e.preventDefault();
+        return false;
+    }
+  });
+
 
   $(window).scroll(function() {
 
@@ -84,11 +96,6 @@ $(document).ready(function() {
       $('body,html').animate({
           scrollTop : 0
       }, 500);
-  });
-
-  $('input[name*=phone], input[name*=fax]').on('input', function(e) {
-    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
-    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
   });
 
 });
